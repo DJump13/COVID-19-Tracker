a1420f0684b2218249aa0b09e5b3c3f70e3750de
David <djump2019@gmail.com>
2025-07-17 14:00:22 -0700

    populated country selection dropdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,76 @@
 import './App.css';
+import { useEffect, useState } from "react";
+import{
+  MenuItem,
+  FormControl,
+  Select
+} from "@mui/material"
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  //State = how to write a variable in REACT
+  //https://disease.sh/v3/covid-19/countries
+
+  //useeffect = runs a piece of code based on a given condition
+
+  useEffect(() => {
+    //the code inside here will run once when the component loads and not again
+    //async -> send a request, wait for it, do something with info
+    const getCountriesData = async () => {
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country, //United States, United Kingdom
+            value: country.countryInfo.iso2 //UK, USA, FR
+          }));
+
+          setCountries(countries);
+      });
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
-      <h1>Let's build a COVID-19 Tracker Clever Programmers!!!</h1>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value="abc">
+              {/* Loop through all the countries and show a dropdown 
+              list of the options */}
+
+              {
+                countries.map(country => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))
+              }
+
+              {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+              <MenuItem value="worldwide">Option two</MenuItem>
+              <MenuItem value="worldwide">Option 3</MenuItem>
+              <MenuItem value="worldwide">YOOOOO</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+      
+
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
